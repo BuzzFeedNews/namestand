be71071d9df86a4aae0204016cdf7123411b36da
Jeremy Singer-Vine <jsvine@gmail.com>
2014-09-09 17:50:20 -0400

    Bump version to 0.0.2

--- namestand/__init__.py
+++ namestand/__init__.py
@@ -2,5 +2,5 @@ from namestand.converters import *
 from namestand.utils import *
 from namestand import patterns
 
-VERSION_TUPLE = (0, 0, 1)
+VERSION_TUPLE = (0, 0, 2)
 VERSION = ".".join(map(str, VERSION_TUPLE))
